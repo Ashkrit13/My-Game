6fcaade5cb9e7df0539545cdaef0868419dddc9a
Ashkrit13 <73723996+Ashkrit13@users.noreply.github.com>
2021-02-12 21:01:26 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+var player, playerImage;
+var enemy1, enemyImage,enemyGroup;
+var wallImage, sideWallImage, wallGroup;
+
+function preload(){
+  playerImage = loadImage("Images/Boy.png");
+  //wallImage = loadImage("Images/wall.jpg");
+  //sideWallImage = loadImage("Images/sideWall.jpg");
+
+}
+
+function setup() {
+  createCanvas(800,800);
+  player = createSprite(50, 750, 50, 50);
+  player.addImage(playerImage);
+  player.scale = 0.1;
+
+  enemy1 = new Enemy(400,400,80,80);
+  enemy2 = new Enemy(500,220,40,40);
+  enemy3 = new Enemy(600,20,60,60);
+  var pos = Math.random(50,700);
+  enemy1.velocityX += pos;
+
+
+  wall1 = new Wall(110,700,225,20);
+  wall2 = new Wall(400,700,225,20);
+  wall3 = new Wall(690,700,225,20);
+  wall4 = new Wall(585,650,20,100);
+  wall5 = new Wall(295,650,20,100);
+  wall6 = new Wall(210,650,20,100);
+  wall7 = new Wall(500,650,20,100);
+  wall8 = new Wall(110,600,225,20);
+  wall9 = new Wall(400,600,225,20);
+  wall10 = new Wall(690,600,225,20);
+  wall11 = new Wall(150,500,150,20);
+  wall12 = new Wall(90,300,20,215);
+  wall13 = new Wall(210,400,20,200);
+  wall14 = new Wall(315,300,225,20);
+  wall15 = new Wall(300,200,400,20);
+  wall16 = new Wall(300,100,400,20);
+  wall17 = new Wall(295,550,20,100);
+  wall18 = new Wall(255,500,100,20);
+  wall19 = new Wall(500,550,20,100);
+  wall20 = new Wall(585,500,190,20);
+  wall21 = new Wall(675,450,20,100);
+  wall22 = new Wall(585,400,190,20);
+  wall23 = new Wall(500,315,15,450);
+  wall24 = new Wall(50,100,100,20);
+  wall25 = new Wall(585,225,20,100);
+  wall26 = new Wall(695,275,225,20);
+  wall27 = new Wall(700,175,225,20);
+  wall28 = new Wall(10,755,20,100);
+  wall29 = new Wall(790,755,20,100);
+}
+
+function draw() {
+  background("black");
+  fill("red");
+  textSize(24);
+  textFont("Times New Roman");
+  text("<-----EXIT",40,50);
+  enemy1.display();
+  wall1.display();
+  wall2.display();
+  wall3.display();
+  wall4.display();
+  wall5.display();
+  wall6.display();
+  wall7.display();
+  wall8.display();
+  wall9.display();
+  wall10.display();
+  wall11.display();
+  wall12.display();
+  wall13.display();
+  wall14.display();
+  wall15.display();
+  wall16.display();
+  wall17.display();
+  wall18.display();
+  wall19.display();
+  wall20.display();
+  wall21.display();
+  wall22.display();
+  wall23.display();
+  wall24.display();
+  wall25.display();
+  wall26.display();
+  wall27.display();
+  wall28.display();
+  wall29.display();
+  if(keyDown("LEFT_ARROW")){
+    player.x -= 3;
+  }
+  if(keyDown("RIGHT_ARROW")){
+    player.x += 3;
+  }
+  if(keyDown("UP_ARROW")){
+    player.y -= 3;
+  }
+  if(keyDown("DOWN_ARROW")){
+    player.y += 3;
+  }
+  drawSprites();
+}
